fb2b80ffc200ec9c79613d1fcfc3d96e48c56e0a
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-02-17 09:03:20 +0000

    [TASK] Raise version to 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ttnews_rss_import/trunk@88380 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,23 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "ttnews_rss_import".
-#
-# Auto generated 23-01-2010 12:18
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ttnews_rss_import".
+ *
+ * Auto generated 17-02-2015 10:02
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'News RSS Importer',
 	'description' => 'Import RSS to news records',
 	'category' => 'module',
 	'author' => 'Steffen Kamper, Xavier Perseguers',
-	'author_email' => 'xavier@typo3.org',
+	'author_email' => 'xavier@causal.ch',
 	'shy' => '',
-	'dependencies' => 'cms,tt_news',
+	'dependencies' => 'tt_news',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => 'modfunc1',
@@ -29,10 +29,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Causal Sàrl',
-	'version' => '1.0.0-dev',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
 			'php' => '5.3.3-5.6.99',
 			'typo3' => '4.5.0-6.2.99',
 			'tt_news' => '3.0.1-3.6.99',
@@ -42,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"489f";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"8b02";s:21:"ext_conf_template.txt";s:4:"e5fe";s:12:"ext_icon.gif";s:4:"f11e";s:17:"ext_localconf.php";s:4:"073c";s:14:"ext_tables.php";s:4:"ac54";s:14:"ext_tables.sql";s:4:"00a1";s:33:"icon_tx_ttnewsrssimport_feeds.gif";s:4:"f11e";s:13:"locallang.xml";s:4:"98b2";s:7:"tca.php";s:4:"88bc";s:31:"classes/class.rssimport_api.php";s:4:"915e";s:40:"classes/class.tx_ttnewsrssimport_api.php";s:4:"f560";s:51:"classes/class.tx_ttnewsrssimport_tceforms_hooks.php";s:4:"3485";s:47:"classes/class.tx_ttnewsrssimport_updatetask.php";s:4:"07a4";s:71:"classes/class.tx_ttnewsrssimport_updatetask_additionalfieldprovider.php";s:4:"acf5";s:45:"cli/class.tx_ttnewsrssimport_cli_dispatch.php";s:4:"918d";s:14:"doc/manual.sxw";s:4:"c979";s:46:"modfunc1/class.tx_ttnewsrssimport_modfunc1.php";s:4:"f177";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"8d5c";s:16:"ext_autoload.php";s:4:"be91";s:21:"ext_conf_template.txt";s:4:"8074";s:12:"ext_icon.gif";s:4:"f11e";s:17:"ext_localconf.php";s:4:"3fc5";s:14:"ext_tables.php";s:4:"ac54";s:14:"ext_tables.sql";s:4:"2fa7";s:33:"icon_tx_ttnewsrssimport_feeds.gif";s:4:"f11e";s:13:"locallang.xml";s:4:"b570";s:7:"tca.php";s:4:"f0b3";s:40:"classes/class.tx_ttnewsrssimport_api.php";s:4:"d0c3";s:51:"classes/class.tx_ttnewsrssimport_tceforms_hooks.php";s:4:"949a";s:47:"classes/class.tx_ttnewsrssimport_updatetask.php";s:4:"55cb";s:71:"classes/class.tx_ttnewsrssimport_updatetask_additionalfieldprovider.php";s:4:"5396";s:45:"cli/class.tx_ttnewsrssimport_cli_dispatch.php";s:4:"918d";s:14:"doc/manual.sxw";s:4:"f873";s:46:"modfunc1/class.tx_ttnewsrssimport_modfunc1.php";s:4:"de86";}',
 	'suggests' => array(
 	),
 );
